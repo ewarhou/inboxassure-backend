ab43ab03aad17c6384c07a4db5e7e9b1ed448166
Amine Warh <ewarhou@gmail.com>
2025-02-05 14:25:37 +0000

    fix: Make is_good migration independent

--- spamcheck/migrations/0012_add_is_good_to_reports.py
+++ spamcheck/migrations/0012_add_is_good_to_reports.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('spamcheck', '0011_auto_20240321_1200'),  # This should match your last migration
+        ('spamcheck', '0001_initial'),
     ]
 
     operations = [
