b4aaa019fc1ba18742848b03b7e94345aae8021c
Amine Warh <ewarhou@gmail.com>
2025-03-03 22:32:53 +0000

    Add backend.inboxassure.app to ALLOWED_HOSTS

--- inboxassure/settings.py
+++ inboxassure/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-koad96i6a2=2^w=@apr^2*4*rxt@0aze*96_e2)e*@rywglp$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inboxassure-backend.imnodev.com', '68.183.98.54', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['inboxassure-backend.imnodev.com', '68.183.98.54', 'localhost', '127.0.0.1', 'backend.inboxassure.app']
 
 
 # Application definition
